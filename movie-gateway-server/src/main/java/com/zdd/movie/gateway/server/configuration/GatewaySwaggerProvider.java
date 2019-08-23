b51105f7ade25018f0901a4514804567c72c4cd9
zhangdandan <17600568326@sina.cn>
2019-08-23 16:17:26 +0800

    删除打印信息

--- movie-gateway-server/src/main/java/com/zdd/movie/gateway/server/configuration/GatewaySwaggerProvider.java
+++ movie-gateway-server/src/main/java/com/zdd/movie/gateway/server/configuration/GatewaySwaggerProvider.java
@@ -37,8 +37,8 @@ public class GatewaySwaggerProvider implements SwaggerResourcesProvider {
                             .filter(predicateDefinition -> ("Path").equals(predicateDefinition.getName()
                             ))
                             .forEach(predicateDefinition -> {
-                                System.out.println(routeDefinition.getId());
-                                System.out.println(predicateDefinition.getArgs().get("\"pattern\"").replace("/**",API_URL));
+//                                System.out.println(routeDefinition.getId());
+//                                System.out.println(predicateDefinition.getArgs().get("\"pattern\"").replace("/**",API_URL));
                                 swaggerResourceList.add(swaggerResource(routeDefinition.getId(),predicateDefinition.getArgs().get("\"pattern\"").replace("/**",API_URL)));
                             });
                 });
